75a4296b42fd202a6f642c530e1e7a8d10a65942
Chuks <ikedichukwubuikem@gmail.com>
2023-03-02 19:09:00 +0100

    brb

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,47 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
+        title: const Text('my first app'),
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-          child: Image.network('https://images.unsplash.com/photo-1677678183663-0c1cd16eb331?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')
+      body: Row(
+        children: [
+          Expanded(
+            child: Image.network('https://images.unsplash.com/photo-1677678183663-0c1cd16eb331?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
+            flex: 3,
+            
+            ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.cyan,
+              child: const Text('1'),
+            ),
           ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.amber,
+              child: const Text('2'),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              color: Colors.pinkAccent,
+              child: const Text('3'),
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.red[600],
-        child: Text('click'),
+        child: const Text('click'),
       ),
     );
   }
